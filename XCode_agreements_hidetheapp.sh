ba2a7d947ad40abebad23bd833acf52a59901963
jpallagrosi <133862364+jpallagrosi@users.noreply.github.com>
2023-10-05 15:33:48 +0100

    Update XCode_agreements_hidetheapp.sh

--- XCode_agreements_hidetheapp.sh
+++ XCode_agreements_hidetheapp.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+#Accept the agreements
 xcodebuild -license accept
 xcode-select -s /Applications/Xcode.app/Contents/Developer
+
+#Hide the app
+chflags hidden /Applications/Xcode.app
+
+exit 0
