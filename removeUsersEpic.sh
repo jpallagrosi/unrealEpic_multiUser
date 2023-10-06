39ebc81f6b5e01ede4c4eda95f818c3178bf5ce9
jpallagrosi <133862364+jpallagrosi@users.noreply.github.com>
2023-10-06 16:22:05 +0100

    Create removeUsersEpic.sh

---
+++ removeUsersEpic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Remove the Epic directory in all users Libraries.
+# This in case Epic is already installed and has been opened by users.
+
+for userName in `ls /Users | grep -v its | grep -v Shared`
+do
+    if [ -d /Users/$userName/Library/Application\ Support/Epic/ ]
+        then 
+        echo "Removing Epic directory"
+        rm -r "/Users/$userName/Library/Application Support/Epic/"
+    else
+        echo "$userName has not opened Epic"
+    fi
+
+done
