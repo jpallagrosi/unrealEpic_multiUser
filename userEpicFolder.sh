de50cc82d94103eb730c1611b4c02a663a660909
jpallagrosi <133862364+jpallagrosi@users.noreply.github.com>
2023-10-09 16:51:06 +0100

    Update userEpicFolder.sh

--- userEpicFolder.sh
+++ userEpicFolder.sh
@@ -2,7 +2,7 @@
 
 for userName in `ls /Users | grep -v its | grep -v Shared`
 do
-    if [ -d /Users/$userName/Library/Application\ Support/Epic/ ]
+    if [[ -d /Users/$userName/Library/Application\ Support/Epic/ ]]
         then 
         echo "<result>Epic Folder</result>"
     else
