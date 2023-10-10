df8afaf0d0b056852c15c13ebffd6cc3c56895f1
jpallagrosi <133862364+jpallagrosi@users.noreply.github.com>
2023-10-10 15:09:42 +0100

    Update z_epicManifests_cp2.sh

--- z_epicManifests_cp2.sh
+++ z_epicManifests_cp2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 loggedInUser=$(ls -l /dev/console | awk '{print$3}')
-localAdmin='"$4'"
-#epicLibrary="/Users/$loggedInUser/Library/Application\ Support/Epic/"
+epicLibrary="/Users/$loggedInUser/Library/Application\ Support/Epic/"
+localAdmin=$4
 
-if [ -d /Users/$loggedInUser/Library/Application\ Support/Epic/ ]
-#if [ -d ${!epicLibrary} ]
+#if [ -d /Users/$loggedInUser/Library/Application\ Support/Epic/ ]
+if [ -d $epicLibrary ]
 	then 
 		echo "Epic directory already exists."
 	else
-		until [ -d /Users/$loggedInUser/Library/Application\ Support/Epic/ ]
-		#until [ -d ${!epicLibrary} ]
+		#until [ -d /Users/$loggedInUser/Library/Application\ Support/Epic/ ]
+		until [ -d $epicLibrary ]
     		do
            		echo "Epic directory not found. Waiting..."
         		sleep 3
@@ -18,10 +18,10 @@ if [ -d /Users/$loggedInUser/Library/Application\ Support/Epic/ ]
         echo "Epic directory found. Proceeding..."
         sleep 3
 
-        mkdir -p "/Users/$loggedInUser/Library/Application Support/Epic/EpicGamesLauncher/Data"
-		#mkdir -p "${!epicLibrary}/EpicGamesLauncher/Data"
-        cp -R "/Users/$localAdmin/Library/Application Support/Epic/EpicGamesLauncher/Data/Manifests" "/Users/$loggedInUser/Library/Application Support/Epic/EpicGamesLauncher/Data/"
-		#cp -R "${!epicLibrary}/EpicGamesLauncher/Data/Manifests" "${!epicLibrary}/EpicGamesLauncher/Data/"
+        #mkdir -p "/Users/$loggedInUser/Library/Application Support/Epic/EpicGamesLauncher/Data"
+	mkdir -p "$epicLibrary/EpicGamesLauncher/Data"
+        #cp -R "/Users/$localAdmin/Library/Application Support/Epic/EpicGamesLauncher/Data/Manifests" "/Users/$loggedInUser/Library/Application Support/Epic/EpicGamesLauncher/Data/"
+	cp -R "$epicLibrary/EpicGamesLauncher/Data/Manifests" "$epicLibrary/EpicGamesLauncher/Data/"
 
 		sleep 7
 		osascript  -e 'display dialog "Please restart the Epic Games Launcher to enable full functionality." buttons {"Restart EPIC"} default button 1'
